624fe28d262d6076b9152e4b846827639b92783d
Chenyang Wu <hpghjfs@yeah.net>
2020-12-08 00:55:54 +0800

    Optimize backup

--- src/planner.jl
+++ src/planner.jl
@@ -24,29 +24,53 @@ function explore!(D::AdaOPSTree, b::Int, p::AdaOPSPlanner)
         b = next_best(D, b, p)
     end
 
-    if D.Delta[b] > p.sol.D
-        make_default!(D, b)
-    end
+    # if D.Delta[b] > p.sol.D
+    #     make_default!(D, b)
+    # end
     return b
 end
 
-function make_default!(D::AdaOPSTree, b::Int)
-    D.u[b] = D.l[b]
-end
+# function make_default!(D::AdaOPSTree, b::Int)
+#     D.u[b] = D.l[b]
+# end
+
 
 function backup!(D::AdaOPSTree, b::Int, p::AdaOPSPlanner)
-    # Note: maybe this could be sped up by just calculating the change in the one mu and l corresponding to bp, rather than summing up over all bp
+    Δu = 0.0
+    Δl = 0.0
+    if D.Delta[b] > p.sol.D
+        Δu = D.l[b] - D.u[b]
+        D.u[b] = D.l[b]
+    end
     while b != 1
-        ba = D.parent[b]
+        bp = b
+        ba = D.parent[bp]
         b = D.ba_parent[ba]
 
-        D.ba_u[ba] = D.ba_r[ba] + discount(p.pomdp) * sum(D.u[bp] * D.obs_prob[bp] for bp in D.ba_children[ba])
-        D.ba_l[ba] = D.ba_r[ba] + discount(p.pomdp) * sum(D.l[bp] * D.obs_prob[bp] for bp in D.ba_children[ba])
+        D.ba_u[ba] += discount(p.pomdp) * D.obs_prob[bp] * Δu
+        D.ba_l[ba] += discount(p.pomdp) * D.obs_prob[bp] * Δl
 
-        D.u[b] = maximum(D.ba_u[ba] for ba in D.children[b])
-        D.l[b] = maximum(D.ba_l[ba] for ba in D.children[b])
+        new_u = maximum(D.ba_u[ba] for ba in D.children[b])
+        new_l = maximum(D.ba_l[ba] for ba in D.children[b])
+        Δu = new_u - D.u[b]
+        Δl = new_l - D.l[b]
+        D.u[b] = new_u
+        D.l[b] = new_l
     end
 end
+# function backup!(D::AdaOPSTree, b::Int, p::AdaOPSPlanner)
+#     # Note: maybe this could be sped up by just calculating the change in the one mu and l corresponding to bp, rather than summing up over all bp
+#     while b != 1
+#         ba = D.parent[b]
+#         b = D.ba_parent[ba]
+
+#         D.ba_u[ba] = D.ba_r[ba] + discount(p.pomdp) * sum(D.u[bp] * D.obs_prob[bp] for bp in D.ba_children[ba])
+#         D.ba_l[ba] = D.ba_r[ba] + discount(p.pomdp) * sum(D.l[bp] * D.obs_prob[bp] for bp in D.ba_children[ba])
+
+#         D.u[b] = maximum(D.ba_u[ba] for ba in D.children[b])
+#         D.l[b] = maximum(D.ba_l[ba] for ba in D.children[b])
+#     end
+# end
 
 function next_best(D::AdaOPSTree, b::Int, p::AdaOPSPlanner)
     max_u = -Inf
